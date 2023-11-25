b6877accdebe396b595990bc86a64dd9117ed4e3
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-11-25 14:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+    let r=document.getElementById("radius").value;
+	let v=document.getElementById("volume");
+	let num=(4/3)*Math.PI*Math.pow(r,3);
+	num=num.toFixed(4);
+	v.value=num;
+	return false:
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
